93e8c3cfa209edaf4c6a73b0762d73ee4f3691fb
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-08-28 23:03:38 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+
+httpServer.listen(3000, () => {
+  console.log(`✅ Server is running on port 3000`);
 });
