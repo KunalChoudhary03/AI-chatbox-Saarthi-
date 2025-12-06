d78199803b9f4539b4b7136ce56dd1e8a218ecdd
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-12-06 13:39:58 +0530

    Upgrade AI model and enhance system instructions
    Updated AI model from 'gemini-2.0-flash' to 'gemini-2.5-flash' with new system instructions for improved user interaction.

--- backend/src/service/ai.service.js
+++ backend/src/service/ai.service.js
@@ -4,12 +4,15 @@ const ai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function generateResponse(chatHistory) {
   try {
-    const model = ai.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-    const result = await model.generateContent({
-      contents: chatHistory,
-    });
-
+    const model = ai.getGenerativeModel({ model: "gemini-2.5-flash",
+      systemInstruction: `
+You are Saarthi AI, a friendly and helpful chatbot created to assist users with technical concepts, coding problems, and general knowledge. 
+The user interacting with you is named Kunal, a full-stack (MERN) developer learning backend development, DSA, and computer networks. 
+Always give clear, simple, example-based explanations. When he asks technical questions, answer step-by-step. 
+If he struggles, guide him patiently. Keep responses accurate, short, and meaningful unless he requests long explanations. 
+Your primary goal is to help him learn efficiently and improve his understanding.
+      ` });
+    const result = await model.generateContent(chatHistory);
     return result.response.text();
   } catch (err) {
     console.error("Gemini API Error:", err);
